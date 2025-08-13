0c3819d4e147921d979645d73a13160df9bbf21a
allyhere <camilomichelettors@gmail.com>
2025-08-12 22:52:21 -0300

    adicionada lógica de adicionar tasks

--- exercicio-04-arrays/script.js
+++ exercicio-04-arrays/script.js
@@ -4,9 +4,45 @@ const taskDelete = document.querySelector("[data-js=task-delete]");
 const taskConfig = document.querySelector("[data-js=task-config]");
 
 const createTaskEntry = (formData) => {
-  const row = document.createElement("dr");
-  const data = document.createElement("dt");
+  const row = document.createElement("tr");
+  for (value of formData) {
+    const data = document.createElement("td");
+    data.innerText = value.at(1);
+    row.appendChild(data);
+  }
+
+  row.appendChild(createTaskDelete());
+
+  return row;
 };
+
+const createTaskDelete = () => {
+  const labelEl = document.createElement("label");
+  labelEl.setAttribute("for", "task-delete");
+  labelEl.innerHTML = "Deletar task";
+
+  const inputEl = document.createElement("input");
+  inputEl.setAttribute("type", "checkbox");
+  inputEl.setAttribute("name", "task-delete");
+  labelEl.appendChild(inputEl);
+
+  const rowContainerEl = document.createElement("td");
+  rowContainerEl.appendChild(labelEl);
+
+  return rowContainerEl;
+};
+
+const addTaskEntry = (template) => {
+  const lastNode = taskForm.querySelector("tbody tr:last-child");
+  const parentElement = lastNode.parentElement;
+  parentElement.insertBefore(template, lastNode);
+};
+
 taskForm.addEventListener("submit", (event) => {
   event.preventDefault();
+  const taskData = new FormData(event.target);
+  const generateTaskTemplate = createTaskEntry(taskData);
+
+  addTaskEntry(generateTaskTemplate);
+  event.target.reset();
 });
