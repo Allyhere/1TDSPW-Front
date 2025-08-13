f6da42f0608b88e28735a2d7351e60f58a44a6ed
allyhere <camilomichelettors@gmail.com>
2025-08-12 21:42:41 -0300

    WIP: Quarto exercício

---
+++ exercicio-04-arrays/script.js
@@ -0,0 +1,12 @@
+const taskForm = document.querySelector("[data-js=task-form]");
+const taskTable = document.querySelector("[data-js=task-table]");
+const taskDelete = document.querySelector("[data-js=task-delete]");
+const taskConfig = document.querySelector("[data-js=task-config]");
+
+const createTaskEntry = (formData) => {
+  const row = document.createElement("dr");
+  const data = document.createElement("dt");
+};
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
